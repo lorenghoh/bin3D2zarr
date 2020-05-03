7746ecbcc6d76d10d0425466456e0db2ea4689b1
Loren Oh <loren.gh.oh@gmail.com>
2020-05-02 22:52:14 -0700

    Ensure full list re-write

--- src/lib/handler.py
+++ src/lib/handler.py
@@ -2,18 +2,17 @@ import json
 
 from .lock import f_lock
 
-def update_dict(key=None, target=0, flag=1):
+def update_dict(key=None, target=4, flag=1):
     with f_lock("bin3D_list.json") as json_file:
         json_dict = json.load(json_file)
 
         if key is None:
             for key in json_dict.keys():
-                if json_dict[key] == target:
+                if json_dict[key] != target:
                     break
         json_dict[key] = flag
     
     with open("bin3D_list.json", 'w+') as json_file:
-        # json_file.seek(0)
         json_file.write(json.dumps(json_dict, indent=4))
 
     return key
